c0ce3b2f81cdddaf200c8b0c3fe06a7d9ad44d1e
SantiagoAjala0204 <santiago.ajala@yachaytech.edu.ec>
2023-07-02 21:21:47 -0500

    comit_cambio_foto_perfil

--- app/controller/Perfil.php
+++ app/controller/Perfil.php
@@ -6,15 +6,38 @@ class Perfil extends Controller
         $this-> perfil= $this->model('perfilUsuario');
         $this-> usuario= $this->model('usuario');
     }
-    public function index()
+    public function index($user)
     {
         if (isset($_SESSION['logueado'])){
-            $datosPerfil= $this->usuario->getPerfil($_SESSION['logueado']);
+            $datosUsuario= $this->usuario->getUsuario("A0204");
+            $datosPerfil= $this->usuario->getPerfil($datosUsuario->idusuario);
             $datos=[
-                'perfil'=> $datosPerfil
+                'perfil'=> $datosPerfil,
+                'usuario'=> $datosUsuario
             ];
 
             $this->view('pages/perfil', $datos);
+        }
+    }
+    public function cambiarImagen()
+    {
+        $carpeta="C:/xampp/htdocs/YachayConnect/public/img/imagenesPefil/";
+        opendir($carpeta);
+        $rutaImagen= 'img/imagenesPefil/' .  $_FILES['imagen']['name'];
+        $ruta=$carpeta . $_FILES['imagen']['name'];
+        copy($_FILES['imagen']['tmp_name'], $ruta);
+        $datos= [
+            'idusuario' => trim($_POST['id_user']),
+            'ruta' => $rutaImagen
+        ];
+
+        $imagenActual= $this->usuario->getPerfil($datos['idusuario']);
+        unlink('C:/xampp/htdocs/YachayConnect/public/' . $imagenActual->fotoPerfil);
+        if ($this->perfil->editarFoto($datos))
+        {
+            redirection('/home');
+        }else{
+            echo 'El perfil no se ha guardado';
         }
     }
 }
--- app/view/pages/perfil.php
+++ app/view/pages/perfil.php
@@ -2,17 +2,76 @@
 
 include_once URL_APP . '/view/custom/header.php';
 include_once URL_APP . '/view/custom/navbar.php';
-#var_dump($datos['usuario']);
-echo "Este es la vista del";
-echo "Este es la vista del";echo "Este es la vista del";
-echo "Este es la vista del";
-echo "Este es la vista del";
-echo "Este es la vista del";
-echo "Este es la vista del";
-
 ?>
 
+<div class="perfil-container-usuario">
+    <div class="imagen-header-perfil-usuario">
+        <img src="<?php echo URL_PROJECT ?>/img/imagenesPefil/imagenes-portada-perfil/cover-img.jpg" class="imagen-portada-perfil" alt="">
+    </div>
+    <div class="container-header-usuario">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="datos-perfil-usuario">
+                        <img src="<?php echo URL_PROJECT?>/<?php echo $datos['perfil']->fotoPerfil?>" class="imagen-perfil-usuario" alt="">
+                        <?php if ($datos['usuario']->idusuario==$_SESSION['logueado']) : ?>
+                            <div class="imagen-perfil-cambiar">
+                                <form action="<?php echo URL_PROJECT?>/perfil/cambiarImagen" method="POST" enctype="multipart/form-data">
+                                    <i class="fas fa-camera"></i> 
+                                    <div class="input-file">
+                                        <input type="hidden" name="id_user" value="<?php echo $_SESSION['logueado']?>">
+                                        <input type="file" name="imagen" id="imagen">
+                                        
+                                    </div>
+                                    <div class="editar-perfil">
+                                        <button class="btn-change-image">Editar</button>
+                                    </div>
+                                </form>
+                            </div>
+                        <?php endif ?>
+                        <div class="datos-personales-usuario">
+                            <h3><?php echo ucwords ($datos['usuario']->usuario)?></h3>
+                            <div class="descripcion-usuario">
+                                <span><?php echo $datos['perfil']->nombreCompleto?></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6"></div>
+                    <div class="container-principal-informacion-usuario">
+                        <div class="container-style-main">
+                            <?php if ($datos['usuario']->idusuario == $_SESSION['logueado']) : ?>
+                                <div class="container-usuario-publicar">
+                                    <a href="<?php echo URL_PROJECT?>/perfil/<?php echo $datos['usuario']->usuario?>"><img src="<?php echo URL_PROJECT . '/' . $datos['perfil']->fotoPerfil ?>" class="image-border" alt=""></a>
+                                    <form action="" class="form-publicar ml-2">
+                                        <textarea name="" id="" class="published mb-0" name="post" placeholder="Que estas pensando?" required></textarea>
+                                        <div class="image-upload-file">
+                                            <div class="upload-photo">
+                                                <img src="<?php echo URL_PROJECT ?>/img/image.png" alt="" class="image-public">
+                                                <div class="input-file">
+                                                    <input type="file" name="imagen" id="imagen">
+                                                </div>
+                                                <span class="ml-1">Subir foto</span>    
+                                            </div>
+                                            <button class="btn-publi">Publicar</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            <?php endif ?>        
+                        </div>
+                    </div>
+                </div>
 
+                <div class="col-md-2">
+                    <div class="container-usuario-contact">
+                        <a href="" class="btn-message"><span class="big"><i class="far fa-envelope"></i></span>Mensaje</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php
 include_once URL_APP . '/view/custom/footer.php';
+var_dump($datos['usuario']);
 ?>
