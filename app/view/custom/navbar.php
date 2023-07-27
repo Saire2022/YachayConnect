a38792a3f66ba88cb6c0a24e02ee3b83dc04b0ea
unknown <saire.conejo@yachaytech.edu.ec>
2023-07-26 21:40:48 -0500

    LastW

--- app/view/custom/navbar.php
+++ app/view/custom/navbar.php
@@ -14,17 +14,17 @@
                 >
                 <i class="fas fa-bars"></i>
                 </button>
-            <a class="navbar-brand" href="<?php echo URL_PROJECT?>/home"><img src="<?php echo URL_PROJECT ?>/img/YT-logo.png" alt="Logo" width="30px" height="30px" class="d-inline-block align-text-top"></a>
+            <a class="navbar-brand btn btn-primary" href="<?php echo URL_PROJECT?>/home"><img src="<?php echo URL_PROJECT ?>/img/YT-logo.png" alt="Logo" width="30px" height="30px" class="d-inline-block align-text-top"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link text-white" href="<?php echo URL_PROJECT?>/home"><span class="big"><i class="fas fa-home mr-1"></i></span>Inicio</a>
+                        <a class="nav-link text-white btn btn-primary" href="<?php echo URL_PROJECT?>/home"><span class="big"><i class="fas fa-home mr-1"></i></span>Inicio</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-white" href="#"><span class="big"><i class="fas fa-users mr-1"></i></span><span class="mb-0 ml-1">Usuarios</span></a>
+                        <a class="nav-link text-white btn btn-primary" href="#"><span class="big"><i class="fas fa-users mr-1"></i></span><span class="mb-0 ml-1">Usuarios</span></a>
                     </li>
                 </ul>
                 <form class="d-flex">
@@ -32,7 +32,7 @@
                     <button class="btn btn-light" type="submit"><i class="fas fa-search mr-1"></i></button>
                 </form>
                 <div class="navbar-nav ml-auto">
-                    <a class="nav-link text-white" href="#"><span class="big"><i class="fa fa-envelope"></i></span><span class="mb-0 ml-1">Mensajes</span></a>
+                    <a class="nav-link text-white btn btn-primary" href="#"><span class="big"><i class="fa fa-envelope"></i></span><span class="mb-0 ml-1">Mensajes</span></a>
                     <a  class="nav-link text-white btn btn-primary" href="<?php echo URL_PROJECT?>/notificaciones">
                         <span class="big"><i class="fa fa-bell"></i></span>
                         <?php if ($datos['misNotificaciones']>0):?>
